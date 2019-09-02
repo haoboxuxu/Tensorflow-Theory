174732b1f34ee95169baf9f271f8afaa838ab023
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2019-09-02 14:52:37 +0800

    Create MNIST数据集分类_1.py

---
+++ MnistTutorial/MNIST数据集分类_1.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+# 批次大小
+batch_size = 128
+n_batch = mnist.train.num_examples // batch_size
+
+x = tf.placeholder(tf.float32, [None,784])
+y = tf.placeholder(tf.float32, [None, 10])
+
+# 创建神经网络
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([1,10]))
+prediction = tf.nn.softmax(tf.matmul(x,W) + b)
+
+# cost fun
+loss = tf.reduce_mean(tf.square(y - prediction))
+
+# GradientDescent
+train_step = tf.train.GradientDescentOptimizer(0.2).minimize(loss)
+
+init = tf.global_variables_initializer()
+
+correct_prediction = tf.equal(tf.arg_max(y,1), tf.arg_max(prediction, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+with tf.Session() as sess:
+    sess.run(init)
+    for epoch in range(21):
+        for batch in range(n_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            sess.run(train_step, feed_dict={x:batch_xs, y:batch_ys})
+        acc = sess.run(accuracy, feed_dict={x:mnist.test.images, y:mnist.test.labels})
+        print("I:" + str(epoch) + " acc=" + str(acc))
