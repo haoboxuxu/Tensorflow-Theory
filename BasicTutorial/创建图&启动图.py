77b643a02f54edf9ec00f1e4df9445eace79094a
haoboxuxu <hoboxuxu@icloud.com>
2019-09-02 09:35:48 +0800

    up

---
+++ BasicTutorial/创建图&启动图.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+m1 = tf.constant([[3,3]])
+m2 = tf.constant([[2],[3]])
+
+product = tf.matmul(m1,m2)
+print(product)
+
+sess = tf.Session()
+res = sess.run(product)
+print(res)
