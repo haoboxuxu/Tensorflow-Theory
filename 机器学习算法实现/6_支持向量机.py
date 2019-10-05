697129387eb82c4d24bfd0ac324db8dd8e47ff4f
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2019-10-05 17:01:06 +0800

    Create 6_支持向量机.py

---
+++ 机器学习算法实现/6_支持向量机.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+import seaborn as sns; sns.set()
+
+# 随机点
+from sklearn.datasets.samples_generator import make_blobs
+X, y = make_blobs(n_samples=50, centers=2, random_state=0, cluster_std=0.6)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+
+# 画线
+xfit = np.linspace(-1, 3.5)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plt.plot([0.6], [2.1], 'x', color='red', markeredgewidth=2, markersize=10)
+
+# 雷区
+for m, b in [(1, 0.65), (0.5, 1.6), (-0.2, 2.9)]:
+    plt.plot(xfit, m * xfit + b, '-k')
+plt.xlim(-1, 3.5);
+
+# Support vector classifier
+from sklearn.svm import SVC
+model = SVC(kernel='linear')
+model.fit(X, y)
+
+print(model.support_vectors_)
